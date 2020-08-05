6789f82277b189e7a1a4afa0e7fde7137c66ec54
James Bradbury <jamesbradbury93@gmail.com>
2020-08-05 12:41:19 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="lcr",
+    version="1.0",
+    author="James Bradbury",
+    author_email="jamesbradbury93@gmail.com",
+    description="Automatic loudness correction.",
+    entry_points={
+        "console_scripts": [
+            "lcr = correct.do"
+        ]
+    }
+)
