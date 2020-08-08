e7c12761e22c8c404d71c0f1589dca5963e4daef
James Bradbury <jamesbradbury93@gmail.com>
2020-08-09 00:57:34 +0100

    add colorize and naughty path handling

--- lcr-nim/src/lcr.nim
+++ lcr-nim/src/lcr.nim
@@ -1,28 +1,42 @@
 # http://k.ylo.ph/2016/04/04/loudnorm.html
-import json, cligen, os, strformat, osproc, strutils
+import json, cligen, os, strformat, osproc, strutils, colorize
 
 let outputFormat = "linear=true:print_format=summary"
 
 proc extractDetails(termLines:seq[string]): JsonNode =
     return termLines[len(termLines)-13..len(termLines)-2].join("").parseJson()
 
-proc entry*(input: seq[string], targetLUFS:float= -14.0, targetRange:float = 7.0, truePeak:float = -1.0, sampleRate:int = 44100): void =
+proc entry*(input: seq[string], 
+    targetLUFS:float= -14.0, 
+    targetRange:float = 7.0, 
+    truePeak:float = -1.0, 
+    sampleRate:int = 44100,
+    verbose:bool = true): void =
     if input.len() == 0:
-        echo "Please provide one or more files to process."
+        echo "Please provide one or more files to process.".bold.fgRed
     for p in input: # iterate overa ll files
-        var inFile = p.absolutePath()
-        var outFile = joinPath(p.splitFile().dir, fmt"{p.splitFile().name}_corrected.wav").absolutePath()
+
+        if verbose: echo fmt"{p.splitFile().name}".fgYellow
+        var inFile = fmt("'{p}'")
+        var outFile = joinPath(p.splitFile().dir, fmt"'{p.splitFile().name}_corrected.wav'")
         
-        var firstPassCmd = fmt("ffmpeg -i {inFile} -filter:a loudnorm=I={targetLUFS}:LRA={targetRange}:truePeak={truePeak}:print_format=json -f null -")
+        var firstPassCmd = fmt("ffmpeg -i {inFile} -filter:a loudnorm=I={targetLUFS}:LRA={targetRange}:TP={truePeak}:print_format=json -f null -")
         var firstPass = osproc.execProcess(firstPassCmd).splitLines()
-        var d = firstPass.extractDetails()
-        echo d
+        echo firstPass
+        var d: JsonNode
+        try:
+            d = firstPass.extractDetails()
+            echo d
+        except JsonParsingError:
+            echo "There was a problem parsing the first pass parameters.".bold.fgRed
+            quit()
+        
         var measuredI = d["input_i"].getStr()
         var measuredLRA = d["input_lra"].getStr()
         var measuredTP = d["input_tp"].getStr()
         var measuredThresh = d["input_thresh"].getStr()
         var offset = d["target_offset"].getStr()
-        var filter = fmt("loudnorm=I={targetLUFS}:LRA={targetRange}:truePeak={truePeak}:measured_I={measuredI}:measured_LRA={measuredLRA}:measured_tp={measuredTP}:measured_thresh={measuredThresh}:offset={offset}:{outputFormat}")
+        var filter = fmt("loudnorm=I={targetLUFS}:LRA={targetRange}:TP={truePeak}:measured_I={measuredI}:measured_LRA={measuredLRA}:measured_tp={measuredTP}:measured_thresh={measuredThresh}:offset={offset}:{outputFormat}")
         var secondPassCmd = fmt("ffmpeg -i {inFile} -filter:a {filter} -ar {sampleRate} -c:a pcm_s32le {outFile}")
         discard os.execShellCmd(secondPassCmd)
 
@@ -32,12 +46,14 @@ when isMainModule:
             "targetLUFS" : 't',
             "targetRange" : 'r',
             "truePeak" : 'p',
-            "sampleRate" : 's'
+            "sampleRate" : 's',
+            "verbose" : 'q'
         },
         help = {
             "input" : "Multiple files or a single file to process",
             "targetLUFS" : "Target LUFS",
             "targetRange" : "Target range",
             "truePeak" : "Maximum true peak",
-            "sampleRate" : "Output sampling rate"
+            "sampleRate" : "Output sampling rate",
+            "verbose" : "Do you want me to tell you what im thinking?"
         })
